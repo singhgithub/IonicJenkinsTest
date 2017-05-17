86662c15e9eacf8e736ea699cf18e91efeca0cbd
redtailsingh <harminder.singh@redtailtechnology.com>
2017-05-17 10:20:06 -0700

    added jest

--- JenkinsTest/src/pages/home/home.ts
+++ JenkinsTest/src/pages/home/home.ts
@@ -3,7 +3,25 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  template: `
+    <ion-header>
+      <ion-navbar>
+        <ion-title>Home</ion-title>
+      </ion-navbar>
+    </ion-header>
+
+    <ion-content padding>
+      <h2>Welcome to Ionic!</h2>
+      <p>
+        This starter project comes with simple tabs-based layout for apps
+        that are going to primarily use a Tabbed UI.
+      </p>
+      <p>
+        Take a look at the <code>src/pages/</code> directory to add or change tabs,
+        update any existing page or create new pages.
+      </p>
+    </ion-content>
+  `
 })
 export class HomePage {
 
@@ -12,3 +30,7 @@ export class HomePage {
   }
 
 }
+
+// export function sum(a, b) {
+//   return a + b;
+// }
