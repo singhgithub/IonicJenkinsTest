7bf689c44b96adc31a315ca54015fbbf6a66d2f8
redtailsingh <harminder.singh@redtailtechnology.com>
2017-05-17 12:00:40 -0700

    new tests added

--- JenkinsTest/src/pages/home/home.js
+++ JenkinsTest/src/pages/home/home.js
@@ -2,3 +2,18 @@ function sum(a, b) {
   return a + b;
 }
 module.exports = sum;
+
+function gethello() {
+  return 'hello';
+}
+module.exports = gethello;
+
+function getworld() {
+  return 'worldd';
+}
+module.exports = getworld;
+
+function gethelloworld() {
+  return 'helloworldd';
+}
+module.exports = gethelloworld;
--- JenkinsTest/src/pages/home/home.test.js
+++ JenkinsTest/src/pages/home/home.test.js
@@ -11,3 +11,15 @@ const sum = require('./home');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('should return hello', () => {
+  expect(gethello()).toBe('hello');
+});
+
+test('should return world', () => {
+  expect(getworld()).toBe('world');
+});
+
+test('should return helloworld', () => {
+  expect(gethelloworld()).toBe('helloworld');
+});
